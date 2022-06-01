77d0508b1ae1e943c856db10843e707c4fc3d9a2
Josef Fatu <ac111299@avcol.school.nz>
2022-06-02 10:43:26 +1200

    fixed the problem with the navigation buttons

---
+++ Hospital Management Database/Data/Migrations/20220601223812_fixed the views for the tables.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hospital_Management_Database.Data.Migrations
+{
+    public partial class fixedtheviewsforthetables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
